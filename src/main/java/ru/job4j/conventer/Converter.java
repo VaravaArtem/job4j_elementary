4f2e2d220531ecd2aa1cac2e6334f861a9a481de
VaravaArtem <varavaartiom@ya.ru>
2024-02-21 11:39:21 +0300

    5. Конвертер валюты

---
+++ src/main/java/ru/job4j/conventer/Converter.java
@@ -0,0 +1,22 @@
+package ru.job4j.conventer;
+
+public class Converter {
+
+    public static float rubleToEuro(float value) {
+        float result = value / 70;
+        return result;
+    }
+
+    public static float rubleToDollar(float value) {
+        float result = value / 60;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        float euro = Converter.rubleToEuro(140);
+        System.out.println("140 rubles are " + euro + " euro.");
+        float dollar = Converter.rubleToDollar(120);
+        System.out.println("120 rubles are " + dollar + " dollar ");
+
+    }
+}
