19824e47087ac7bd6f50505bfb3550565c76ed5f
VaravaArtem <varavaartiom@ya.ru>
2024-03-06 22:45:01 +0300

    1. Модульные тесты

--- src/test/java/ru/job4j/ConventerTest.java
+++ src/test/java/ru/job4j/ConventerTest.java
@@ -3,7 +3,7 @@ package ru.job4j;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
-public class MainTest {
+public class ConventerTest {
     @Test
     public void whenActionThenResult() {
         int excepted = 1;
