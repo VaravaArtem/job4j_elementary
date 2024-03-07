6fab5082c9737bb289f55b9ea81b2e967daafbc8
VaravaArtem <varavaartiom@ya.ru>
2024-03-07 19:26:47 +0300

    1. Модульные тесты.

---
+++ src/test/java/ru/job4j/conventer/ConverterTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.conventer;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.BDDAssertions.withPrecision;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConverterTest {
+
+    @Test
+    public void rubleToEuro() {
+        float input = 140;
+        float expected = 2;
+        float output = Converter.rubleToEuro(input);
+        float value = 0.0001f;
+        Assertions.assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+
+    @Test
+    public void rubleToDollar() {
+        float input = 180;
+        float expected = 3;
+        float output = Converter.rubleToDollar(input);
+        float value = 0.0001f;
+        assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+}
