2e50accb4807ec3539ccb8054376ed9299c46796
VaravaArtem <varavaartiom@ya.ru>
2024-04-03 19:06:15 +0300

    1. Модульные тесты.

--- src/test/java/ru/job4j/conventer/ConverterTest.java
+++ src/test/java/ru/job4j/conventer/ConverterTest.java
@@ -2,11 +2,9 @@ package ru.job4j.conventer;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.BDDAssertions.withPrecision;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ConverterTest {
 
