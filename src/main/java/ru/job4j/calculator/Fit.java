80f48bb8a59efb55fe1e671b81d750939aade6a6
VaravaArtem <varavaartiom@ya.ru>
2024-04-08 17:59:04 +0300

    2. Тесты для задачи "идеальный вес"

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -17,6 +17,6 @@ public class Fit {
         double man = Fit.manWeight(height);
         double woman = Fit.womanWeight(height);
         System.out.println("Man 187 is " + man);
-        System.out.println("Man 187 is " + woman);
+        System.out.println("Woman 187 is " + woman);
     }
 }
